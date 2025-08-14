1e30359297718d4868d4ffc8319a4c22f83160f2
sudonym_ <bailorjacrey@gmail.com>
2025-08-14 01:06:46 -0500

    Set some env variables and make an alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 set fish_greeting
 
+set EDITOR nvim
+set PATH "$PATH:/home/bailor/.local/bin"
+
+alias lg lazygit
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
