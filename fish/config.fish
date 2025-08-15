04b5be78fd6a30c12101216351271e85a8c46abd
sudonym_ <bailorjacrey@gmail.com>
2025-08-14 23:58:39 -0500

    Add the best nix develop abbreviation ever

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ set EDITOR nvim
 set PATH "$PATH:/home/bailor/.local/bin"
 
 alias lg lazygit
+abbr --add ndvp nix develop -c "$SHELL"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
