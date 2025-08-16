2d7c474dcdf100ce6283c53e2c4b9e0c69a15f25
sudonym_ <bailorjacrey@gmail.com>
2025-08-15 21:59:30 -0500

    Move some interactive prompt stuff to the appropriate block
    silly me lol

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,10 @@
 set fish_greeting
 
 set EDITOR nvim
-set PATH "$PATH:/home/bailor/.local/bin"
-
-alias lg lazygit
-abbr --add ndvp nix develop -c "$SHELL"
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    set PATH "$PATH:/home/bailor/.local/bin"
+
+    alias lg lazygit
+    abbr --add ndvp nix develop -c "$SHELL"
 end
