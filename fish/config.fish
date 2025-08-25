cbffe16f60f01a361112fb8e852b258945c807fd
sudonym_ <bailorjacrey@gmail.com>
2025-08-25 14:23:23 -0500

    Created a custom cht.sh command

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,12 @@ if status is-interactive
     set PATH "$PATH:/home/bailor/.local/bin"
     fish_vi_key_bindings
 
+    function cht.sh
+        set -l output "$(gum spin --title="Querying..." --show-output -- curl -s $(string join "" "cht.sh/" "$argv"))"
+        echo $output
+    end
+    funcsave -q cht.sh
+
     alias lg lazygit
     abbr --add ndvp nix develop -c "$SHELL"
     abbr --add nix-search gum spin --show-output --title="Searching..." nix-search $argv
